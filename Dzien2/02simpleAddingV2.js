c65fbbc73391d66dacba682971c73b23f8c56e21
Brux <bruki28@gmail.com>
2022-11-20 08:29:44 +0100

    02simpleAddingV2

--- Dzien2/02simpleAddingV2.js
+++ Dzien2/02simpleAddingV2.js
@@ -1,8 +1,11 @@
 const simpleAdding = (num) => {
   let total = 0;
   // Poczatek kodu
+  for (let i = 1; i <= num; i++) {
+    total += i;
+  }
 
   // Koniec kodu
   return total;
 };
-console.log(simpleAdding(4));
+console.log(simpleAdding(140));
