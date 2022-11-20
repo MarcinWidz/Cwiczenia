52a5c24013156e042b36ef1015286d004cefc013
Brux <bruki28@gmail.com>
2022-11-20 16:58:39 +0100

    box00

--- Dzien3/02cwiczenia/box00.js
+++ Dzien3/02cwiczenia/box00.js
@@ -1,12 +1,47 @@
+const box = (x, y) => {
+  let str = "";
+
+  // Poczatek
+  for (let j = 1; j <= y; j++) {
+    let i = 1;
+
+    while (i <= x) {
+      if ((i == 1 || i == x) && (j == 1 || j == y)) {
+        str += "o";
+      } else {
+        if (j > 1 && j < y) {
+          if (i > 1 && i < x) {
+            str += " ";
+          } else {
+            str += "|";
+          }
+        } else {
+          str += "-";
+        }
+      }
+      if (i == x) {
+        str += "\n";
+      }
+      i++;
+    }
+  }
+
+  // Koniec
+
+  return str;
+};
+
+const x = 10;
+const y = 6;
+console.log(box(x, y));
+
 // console.log(box(5, 3));
 
-// Ma zwrocic: 
+// Ma zwrocic:
 
 // o---o
 // |   |
 // o---o
-
-
 
 // console.log(box(5, 1));
 
@@ -14,13 +49,11 @@
 
 // o---o
 
-
 // console.log(box(1, 1));
 
 //Ma zwrocic
 
 //o
-
 
 //console.log(box(1, 5));
 
@@ -34,7 +67,7 @@
 
 //console.log(box(4, 4));
 
-//Ma zwrocic: 
+//Ma zwrocic:
 
 // o--o
 // |  |
