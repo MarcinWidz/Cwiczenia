09e36b5eb2da94757cd81ee8b4c8888c38506748
Brux <bruki28@gmail.com>
2022-11-20 17:06:39 +0100

    box01

--- Dzien3/02cwiczenia/box01.js
+++ Dzien3/02cwiczenia/box01.js
@@ -1,12 +1,51 @@
+const box = (x, y) => {
+  let str = "";
+
+  // Poczatek
+  for (let j = 1; j <= y; j++) {
+    let i = 1;
+
+    while (i <= x) {
+      if ((i == 1 || i == x) && (j == 1 || j == y)) {
+        if ((i == 1 && j == 1) || (i == x && j == y)) {
+          str += "/";
+        } else {
+          str += "\\";
+        }
+      } else {
+        if (j > 1 && j < y) {
+          if (i > 1 && i < x) {
+            str += " ";
+          } else {
+            str += "*";
+          }
+        } else {
+          str += "*";
+        }
+      }
+      if (i == x) {
+        str += "\n";
+      }
+      i++;
+    }
+  }
+
+  // Koniec
+
+  return str;
+};
+
+const x = 1;
+const y = 1;
+console.log(box(x, y));
+
 // console.log(box(5, 3));
 
-// Ma zwrocic: 
+// Ma zwrocic:
 
 /***\
 *   *
 \***/
-
-
 
 // console.log(box(5, 1));
 
