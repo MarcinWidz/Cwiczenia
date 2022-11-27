87352ce2d868d5a4712d49174693e4c3865a1879
Brux <bruki28@gmail.com>
2022-11-27 13:48:49 +0100

    02DisplayArray2

--- Dzien4/01DisplayArray.js
+++ Dzien4/01DisplayArray.js
@@ -2,4 +2,8 @@
 
 // Utwórz funkcję displayArray(tab), która jako argument otrzyma tablicę ciągów znaków i zwróci ją w postaci ciągu znaków.
 
+function displayArray(tab) {
+  return tab;
+}
+
 console.log(displayArray(["Bolon", "Spala", "Penis Grzegorz Sztuczny"])); // Ma zwrocic "Bolon Spala Grzegorz Penis Sztuczny"
--- Dzien4/02DisplayArray2.js
+++ Dzien4/02DisplayArray2.js
@@ -2,5 +2,13 @@
 
 // Utwórz funkcję displayArray(tab, opcja), która jako parametr otrzyma tablicę ciągów znaków i zwróci ją w postaci ciągu znaków. Drugi parametr włączy separator "i" przed ostatnim elementem, czyli.
 
+function displayArray(tab, opcja) {
+  if (opcja === false) {
+    tab = ["Bolon", "Spala", "Penis Grzegorz Sztuczny"];
+  } else if (opcja === true) {
+    tab = ["Bolon1", "Spala1", "Penis Grzegorz Sztuczny1"];
+  }
+  return tab;
+}
 console.log(displayArray(["Bolon", "Spala", "Penis Grzegorz Sztuczny"], false)); // Ma zwrocic "Bolon Spala Grzegorz Penis Sztuczny"
 console.log(displayArray(["Bolon", "Spala", "Penis Grzegorz Sztuczny"], true)); // Ma zwrocic "Bolon, Spala i Grzegorz Penis Sztuczny"
